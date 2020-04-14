df4c3e6270846b1bc9f7672e62b942eb38b2f2be
Natsulelou <nut_ohlanla@hotmail.com>
2020-04-14 21:56:30 +0700

    layout image

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  images: Array<any> = []
+
   constructor() { }
 
   ngOnInit() {
+    this.images = [
+      {
+        "title": "sa",
+        "url": "../../assets/images/gif/SA.gif"
+      },
+      {
+        "title": "dg",
+        "url": "../../assets/images/gif/DG.gif"
+      },
+      {
+        "title": "ag",
+        "url": "../../assets/images/gif/AG.gif"
+      },
+      {
+        "title": "sb",
+        "url": "../../assets/images/gif/SB.gif"
+      },
+      {
+        "title": "xo",
+        "url": "../../assets/images/gif/xo.gif"
+      },
+      {
+        "title": "22",
+        "url": "../../assets/images/gif/22.gif"
+      },
+      {
+        "title": "sp",
+        "url": "../../assets/images/gif/sp.gif"
+      },
+      {
+        "title": "ds",
+        "url": "../../assets/images/gif/ds.gif"
+      },
+      {
+        "title": "ab",
+        "url": "../../assets/images/gif/ab.gif"
+      }
+    ]
   }
 
 }
